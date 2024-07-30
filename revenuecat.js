fc11b45b42c962976f703dc432baadf29f6c16a7
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-07-30 10:53:56 +0300

    Update revenuecat.js

--- revenuecat.js
+++ revenuecat.js
@@ -20,7 +20,7 @@ https://appraven.net/collection/77299969
 [rewrite_local]
 
 # ～ RevenueCat@ddgksf2013
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/revenuecat.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/revenuecat.js 
 ^https:\/\/api\.revenuecat\.com\/.+\/(receipts|subscribers) url script-request-header https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/deleteHeader.js
 
 [mitm]
