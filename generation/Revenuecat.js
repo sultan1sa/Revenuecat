e2566542a89bedb6575ff67c44717c5f21316fb7
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-07-30 16:39:27 +0300

    Update Revenuecat.js

--- generation/Revenuecat.js
+++ generation/Revenuecat.js
@@ -11,8 +11,8 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/SexActions.js
-^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/Revenuecat/SexActions.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/generation/Revenuecat.js
+^https?:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/generation/Revenuecat.js
 
 [mitm]
 hostname = api.revenuecat.com
