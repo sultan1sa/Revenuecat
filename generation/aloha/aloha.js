542396cfc134de98fecd228139bf05e2a08f8346
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-08-01 20:06:10 +0300

    Update aloha.js

---
+++ generation/aloha/aloha.js
@@ -0,0 +1,33 @@
+#!name=Aloha浏览器
+
+#!desc=Aloha VPN浏览器会员解锁
+
+
+
+
+https://api.alohaprofile.com/v1/profile_info url script-response-body https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/generation/aloha/aloha.js
+
+
+
+
+
+hostname = api.alohaprofile.com
+
+
+
+
+{
+  "profile" : {
+    "is_premium" : true,
+    "has_2fa" : true,
+    "id" : 89945,
+    "email_verified" : true,
+    "_end_of_premium" : "2024-08-01 18:02:54",
+    "end_of_premium" : 1722535374,
+    "email" : "sultan1saas@gmail.com",
+    "has_oauth" : true,
+    "token" : "fad33d7908652a21c69913dad0050c04",
+    "can_mint" : false
+  },
+  "error" : null
+}
