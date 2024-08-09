c2221d4836ed70601b023002665b00882c851625
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-08-09 04:31:05 +0300

    Update anybox.js

--- generation/anybox/anybox.js
+++ generation/anybox/anybox.js
@@ -1,58 +1,38 @@
-*******************************/
+/*
+
+Anybox：https://apps.apple.com/app/id1593408455
 
 [rewrite_local]
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-response-body https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/generation/anybox/anybox.js
+^https?:\/\/api\.revenuecat\.com\/v1\/(subscribers\/[^\/]+$|receipts$) url script-request-header https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/generation/anybox/anybox.js
 
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/(.*?)*$) url script-response-body https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/generation/anybox/anybox.js
-
-[mitm] 
-
+[MITM]
 hostname = api.revenuecat.com
 
-*******************************/
-
-
-var objc = JSON.parse($response.body);
-
-    objc = 
-{
-  "request_date_ms" : 1665733801390,
-  "request_date" : "2022-10-14T07:50:01Z",
-  "subscriber" : {
-    "non_subscriptions" : {
-
-    },
-    "first_seen" : "2022-10-14T07:45:36Z",
-    "original_application_version" : "47",
-    "other_purchases" : {
-
-    },
-    "management_url" : "https://apps.apple.com/account/subscriptions",
-    "subscriptions" : {
-      "cc.anybox.Anybox.lifetime" : {
-        "is_sandbox" : false,
-        "ownership_type" : "PURCHASED",
-        "billing_issues_detected_at" : null,
-        "period_type" : "annual",
-        "expires_date" : "2029-10-14T07:49:44Z",
-        "grace_period_expires_date" : null,
-        "unsubscribe_detected_at" : null,
-        "original_purchase_date" : "2022-10-14T07:49:45Z",
-        "purchase_date" : "2022-10-14T07:49:44Z",
-        "store" : "app_store"
-      }
-    },
-    "entitlements" : {
-      "pro" : {
-        "grace_period_expires_date" : null,
-        "purchase_date" : "2022-10-14T07:49:44Z",
-        "product_identifier" : "cc.anybox.Anybox.lifetime",
-        "expires_date" : "2029-10-14T07:49:44Z"
-      }
-    },
-    "original_purchase_date" : "2022-10-14T07:26:13Z",
-    "original_app_user_id" : "$RCAnonymousID",
-    "last_seen" : "2022-10-14T07:45:36Z"
-  }
+*/
+const guding = {};
+const guding6 = JSON.parse(typeof $response != "undefined" && $response.body || null);
+if (typeof $response == "undefined") {
+  delete $request.headers["x-revenuecat-etag"];
+  delete $request.headers["X-RevenueCat-ETag"];
+  guding.headers = $request.headers;
+} else if (guding6 && guding6.subscriber) {
+  guding6.subscriber.subscriptions = guding6.subscriber.subscriptions || {};
+  guding6.subscriber.entitlement = guding6.subscriber.entitlement || {};
+  const app = 'gd';const list = {'gd':{name: 'pro', id: 'cc.anybox.Anybox.lifetime'}};
+  const data = {
+    "expires_date": "6666-06-06T06:06:06Z",
+    "original_purchase_date": "2023-02-23T02:33:33Z",
+    "purchase_date": "2023-02-23T02:33:33Z",
+    "ownership_type" : "PURCHASED",
+    "store" : "app_store"};
+for (const i in list) { if (new RegExp(`^${i}`, `i`).test(app)) {
+guding6.subscriber.subscriptions[list[i].id] = data;
+guding6.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+guding6.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+                break;
+          }
+    }
+    guding.body = JSON.stringify(guding6);
 }
-
-$done({body : JSON.stringify(objc)});
+$done(guding);
