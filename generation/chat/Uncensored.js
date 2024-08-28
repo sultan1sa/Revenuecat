b6779060f4ed3920355899051488791cad44f96e
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-08-28 15:06:42 +0300

    Update Uncensored.js

--- generation/chat/Uncensored.js
+++ generation/chat/Uncensored.js
@@ -3,7 +3,7 @@
 
 
 [rewrite_local]
-AppAdTesters = type=http-response,pattern=https://api.adapty.io/api/v1/sdk/analytics/profiles,requires-body=1,max-size=0,binary-body-mode=0,script-path=https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/generation/Revenuecat.js,script-update-interval=5
+AppAdTesters = type=http-response,pattern=https://api.adapty.io/api/v1/sdk/analytics/profiles,requires-body=1,max-size=0,binary-body-mode=0,script-path=https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/chat/Uncensored.js,script-update-interval=5
 
 [MITM]
 hostname = api.adapty.io
