995349f53ae1736014b8fc71f79ad7267893d23b
sultan1sa <68920908+sultan1sa@users.noreply.github.com>
2024-08-28 15:10:19 +0300

    Update chatUncensored.js

---
+++ generation/evoai/chatUncensored.js
@@ -0,0 +1,6 @@
+[rewrite_local]
+https://api.adapty.io/api/v1/sdk/in-apps/apple/receipt/validate/ url script-response-body https://raw.githubusercontent.com/sultan1sa/Revenuecat/main/generation/evoai/chatUncensored.js
+
+
+[mitm]
+hostname = api.adapty.io
